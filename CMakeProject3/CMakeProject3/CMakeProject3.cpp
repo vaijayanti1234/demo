8520beb11845d13ed14de859f9102a002a54ffc0
vaijayanti1234 <56797883+vaijayanti1234@users.noreply.github.com>
2019-10-30 11:48:50 +0530

    Add files via upload

---
+++ CMakeProject3/CMakeProject3/CMakeProject3.cpp
@@ -0,0 +1,13 @@
+﻿// CMakeProject3.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject3.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	getchar();
+	return 0;
+}
